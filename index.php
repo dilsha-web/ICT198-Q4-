d562c8dee9d1152d68817b81ddc7fdc5f29e1f01
ICT198 <rameshi.wijesinghe@gmail.com>
2022-01-11 13:56:48 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+class StudentParent{
+
+    public $stdName;
+    public $stdRegnum;
+    
+    Public function StudentParent($Name,$regnum){
+        $this->stdName = $stuName;
+        $this->stdRegnum = $regnum;
+        }
+    
+    
+       
+    }
+?>
